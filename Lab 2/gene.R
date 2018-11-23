3799f47bffd58bac7f613a6697b99080b3797af8
DucDuong92 <minhducduong92@gmail.com>
2018-11-23 16:10:16 +0100

    fix 1.1 and add 2.1

--- Lab 2/gene.R
+++ Lab 2/gene.R
@@ -4,27 +4,23 @@ library(seqinr)
 #1.1
 lizards_sequences <-  read.fasta("lizard_seqs.fasta")
 
-simulate_gene <- function(template_gene){
+simulate_gene <- function(template_gene)
+  {
   ai_gene <- list()
   gene_num <- length(template_gene)
   nucleotide <- c("a", "t", "g", "c")
   
   #Scan all gene and get some information
   for (i in 1:gene_num) {
+    
     template_sequence <- template_gene[[i]]
     #get leng and base compotision of gene
     this_leng <- length(template_sequence)
     this_compotision = seqinr::count(template_sequence,1)/this_leng
-    this_sequence <- c()
     
-    # create a number of each nucleotide based on the template_sequence 
-    for (j in 1:4) {
-      letter <- sample(nucleotide[j], size = this_compotision[[j]]*this_leng, replace = TRUE)
-      this_sequence <- c(this_sequence, letter)
-    }
-    #random order
-    rand <- sample(length(this_sequence))
-    this_sequence <- this_sequence[rand]
+    #generate a new sequence base on sample function
+    this_sequence <- sample(nucleotide, prob = this_compotision, replace = TRUE)
+    print(this_sequence)
     
     #add to list
     ai_gene[i] <- list(this_sequence)
@@ -35,19 +31,22 @@ simulate_gene <- function(template_gene){
   return("Created an AI gene and saved in file: AI_gene.fasta")
 }
 
+
 simuate_gene(lizards_sequences)
+ai_gene_1.1 <- read.fasta("AI_gene.fasta")
 
 #1.2
 tree <- rtree(length(lizards_sequences))
 plot(tree)
 
-
+#the rates matrix
 rates <- matrix(0, ncol = 4, nrow = 4)
 rownames(rates) <- c("a", "c", "g", "t")
 colnames(rates) <- c("a", "c", "g", "t")
 
+#fill value to rates matrix
 for (i in 1:4) {
-  rate = runif(3, 0.2, 0.3)
+  rate = runif(3, 0.22, 0.28)
   for (j in 1:4) {
     if (j==4) 
       {
@@ -56,15 +55,12 @@ for (i in 1:4) {
   }
 }
 
+#print the rates
 rates
+#create the ai_gene 2
+ai_gene_1.2 <- simSeq(tree, l = 1000, Q=rates , type = "DNA")
 
-ai_gene_1.2 = list()
-for (i in 1:length(lizards_sequences)) {
-  this_leng <- length(lizards_sequences[[i]])
-  ai_sequence2 <- simSeq(tree, l = this_leng, Q=rates , type = "DNA")
-  ai_gene_1.2[i] <- ai_sequence2
-}
-
+#rename 
 for (i in 1:length(ai_gene_1.2)){
   ai_gene_1.2[[i]][ai_gene_1.2[[i]] == "1"] = "a"
   ai_gene_1.2[[i]][ai_gene_1.2[[i]] == "2"] = "c"
@@ -74,6 +70,21 @@ for (i in 1:length(ai_gene_1.2)){
 
 ape::write.dna(ai_gene_1.2, file ="AI_gene2.fasta", format = "fasta", colsep =" ")
 
+#2.1
+ai_gene_1.2 <- read.fasta("AI_gene2.fasta")
+
+for (i in 1:length(lizards_sequences)) {
+  cat(paste("For the sequences number: ", i , "\n"))
+  print("The composition of lizards dataset:")
+  print(round(count(lizards_sequences[[i]],2)/length(lizards_sequences[[i]]), 4))
+  
+  print("The composition of AI_gene1 dataset:")
+  print(round(count(ai_gene_1.1[[i]],2)/length(ai_gene_1.1[[i]]), 4))
+  
+  print("The composition of Ai_gene2 dataset:")
+  print(round(count(ai_gene_1.2[[i]],2)/length(ai_gene_1.2[[i]]), 4))
+  
+  cat("\n")
+}
 
 
-# 2.2
