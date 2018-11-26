8739af1750e693ea1d2829162becda26e519668e
rryesuafuga <sseguya256@gmail.com>
2018-11-26 09:53:58 +0100

    for question 1 so far

--- Lab2/Lab2_Bioinformatics.R
+++ Lab2/Lab2_Bioinformatics.R
@@ -1,6 +1,7 @@
 
 library(ape)
 library(seqinr)
+library(phangorn)
 
 
 lizard_seq_seqinr <- read.fasta(file = "lizard_seqs.fasta", seqtype = "DNA",
@@ -9,6 +10,27 @@ lizard_seq_seqinr <- read.fasta(file = "lizard_seqs.fasta", seqtype = "DNA",
 # Assignment 1
 
 ## Question 1.1
+
+### Martin's method
+lizards_sequences <- read.fasta("lizard_seqs.fasta")
+lizard_artificial <- lizards_sequences
+nucleotide <- c("a", "c", "g", "t")
+for(i in 1:33){
+  length_s <- length(lizards_sequences[[i]])
+  composition = seqinr::count(lizards_sequences[[i]],1)/length_s
+  lizard_artificial[[i]] <- sample(nucleotide, size = length_s, 
+                                   prob=composition, replace=TRUE)
+  names(lizard_artificial)[i] <- paste0("lizard",i)
+  
+}
+print(lizard_artificial)
+
+ape::write.dna(lizard_artificial, file ="lizard_artificial.fasta", 
+               format = "fasta", append =FALSE, nbcol = 6, colsep = " ", colw = 10)
+
+
+
+### My method
 artificialseq <- list()
 set.seed(12345)
 for(i in 1:length(lizard_seq_seqinr)){
@@ -21,19 +43,52 @@ for(i in 1:length(lizard_seq_seqinr)){
   
 }
 
-# need to change writing method
-ape::write.dna(artificialseq, file ="artificial_lizard_seqs.fasta", format = "fasta", append =FALSE, nbcol = 6, colsep = " ", colw = 10)
+
+write.fasta(sequences=artificialseq,
+            names=names(artificialseq),
+            file.out="artificial_lizard_seqs.fasta")
+
+
+### Comment on base composition
 
 
 
 ## Question 1.2
-second_artificialseq <- list()
+lizard_artificial_2 <- lizards_sequences
+nucleotide <- c("a", "c", "g", "t")
 for(i in 1:33){
-  
-  second_artificialseq[[i]] <- sample(lizards_sequences[[i]], 
-                               size=length(lizards_sequences[[i]]))
-  
+  length_s <- length(lizards_sequences[[i]])
+  composition = seqinr::count(lizards_sequences[[i]],1)/length_s
+  lizard_artificial_2[[i]] <- sample(nucleotide, size = length_s, 
+                                   prob=composition, replace=TRUE)
+  names(lizard_artificial_2)[i] <- paste0("lizard",i)
+
 }
+print(lizard_artificial_2)
+
+ape::write.dna(lizard_artificial_2, file ="lizard_artificial_2.fasta", 
+               format = "fasta", append =FALSE, nbcol = 6, colsep = " ", colw = 10)
+
+
 
 tree <- rtree(n = 33)
 plot(tree)
+l2 <- length(unlist(lizards_sequences))
+bc2 <- seqinr::count(unlist(lizards_sequences),1)/length(unlist(lizards_sequences))
+Q2 <- matrix(c(0.9,0.05,0.03,0.02,0.05,0.87,0.07,0.01,
+               0.03,0.07,0.89,0.01,0.02,0.01,0.01,0.96), 
+             ncol = 4, nrow = 4)
+from_tree <- simSeq(tree, l = l2, type="DNA", bf=as.vector(bc2), Q=Q2)
+
+write.fasta(sequences=from_tree,
+            names=names(lizard_artificial_2),
+            file.out="from_tree.fasta")
+
+### will come back to this later
+
+
+
+
+# Question 2
+
+## 
