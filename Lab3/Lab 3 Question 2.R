4a008677fdc182bf1d1660b8efad6db1688fd1f9
rryesuafuga <sseguya256@gmail.com>
2018-12-06 00:36:28 +0100

    changes until 2.2a

---
+++ Lab3/Lab 3 Question 2.R
@@ -0,0 +1,72 @@
+
+
+# ------------------ Question 2 Lab 3 ----------------------
+
+
+# Question 2
+
+## 2.1
+library(ade4)
+data(carni70)
+View(carni70)
+str(carni70)
+
+carni70_1 <- carni70[[1]]
+carni70_2 <- carni70[[2]]
+
+library(tidyverse)
+
+ggplot(data = carni70_2)+
+  layer(mapping = aes(x=size, y=range), geom = "point", stat = "identity",
+        position = "identity")
+
+ggplot(data = carni70_2)+
+  layer(mapping = aes(x=rownames(carni70_2), y=size), geom = "bar", 
+        stat = "identity", position = "identity")+coord_flip()
+
+ggplot(data = carni70_2)+
+  layer(mapping = aes(x=rownames(carni70_2), y=range), geom = "bar", 
+        stat = "identity", position = "identity")
+
+### generally, they have small sizes
+### Ursus_arctos has the biggest size
+### package meant for multivariate data analysis.
+
+library(ape)
+tree <- read.tree(text=carni70_1)
+plot(tree)
+
+
+
+## 2.2
+
+### 2.2a Both traits evolve as independent Brownian motions
+is.binary.tree(tree)
+tree<-multi2di(tree)
+size2 <- pic(carni70_2$size, tree)
+range2 <- pic(carni70_2$range, tree)
+
+z <- lm(size2 ~ range2 - 1)    # the "-1" forces line through origin
+summary(z)
+correlationz <- sqrt(summary(z)$r.squared)
+## no correlation
+
+bm.tree = fastBM(tree, a=0, sig2=1.0, internal = TRUE)
+phenogram(tree, bm.tree, spread.labels = TRUE)
+
+vcv.phylo(tree, cor=TRUE)
+b <- vcv.phylo(tree, cor=TRUE)
+inv.b <- solve(b)
+
+
+
+### 2.2b The traits evolve as a correlated Brownian motion
+
+
+
+### 2.2c independent Ornstein Uhlenbeck processes
+
+
+
+
+
---
+++ Lab3/SylviaWarblers_reduced.R
@@ -0,0 +1,216 @@
+
+# ---------------------- Lab 3 Question 1 ----------------
+
+library(ape)
+
+###
+### Chapter 3
+###
+
+x <- paste("AJ5345", 26:49, sep = "")
+x <- c("Z73494", x)
+sylvia.seq <- read.GenBank(x)
+
+sylvia.clus <- clustal(sylvia.seq)
+library(phyloch)
+sylvia.maff <- mafft(sylvia.seq, path = "C:/Users/rsseg/Downloads/mafft-win/mafft")
+identical(sylvia.clus[x, ], sylvia.maff[x, ])
+
+taxa.sylvia <- attr(sylvia.seq, "species")
+names(taxa.sylvia) <- names(sylvia.seq)
+rm(sylvia.seq)
+taxa.sylvia[1] <- "Sylvia_atricapilla"
+taxa.sylvia[24] <- "Sylvia_abyssinica"
+
+sylvia.eco <- read.table("sylvia_data.txt")
+str(sylvia.eco)
+rownames(sylvia.eco)
+save(sylvia.clus, taxa.sylvia, sylvia.eco,
+     file = "sylvia.RData")
+
+###
+### Chapter 5
+###
+
+syl.K80 <- dist.dna(sylvia.clus, pairwise.deletion = TRUE)
+syl.F84 <- dist.dna(sylvia.clus, model = "F84", p = TRUE)
+syl.TN93 <- dist.dna(sylvia.clus, model = "TN93", p = TRUE)
+syl.GG95 <- dist.dna(sylvia.clus, model = "GG95", p = TRUE)
+
+round(cor(cbind(syl.K80, syl.F84, syl.TN93, syl.GG95)), 3)
+
+syl.JC69 <- dist.dna(sylvia.clus, model = "JC69", p = TRUE)
+syl.raw <- dist.dna(sylvia.clus, model = "raw", p = TRUE)
+layout(matrix(1:2, 1))
+plot(syl.JC69, syl.raw)
+abline(b = 1, a = 0) # draw x = y line
+plot(syl.K80, syl.JC69)
+abline(b = 1, a = 0)
+
+layout(matrix(1:3, 1))
+for (i in 1:3) {
+    s <- logical(3); s[i] <- TRUE
+    x <- sylvia.clus[, s]
+    d <- dist.dna(x, p = TRUE)
+    ts <- dist.dna(x, "Ts", p = TRUE)
+    tv <- dist.dna(x, "Tv", p = TRUE)
+    plot(ts, d, xlab = "Number of Ts or Tv", col = "blue",
+         ylab = "K80 distance", xlim = range(c(ts, tv)),
+         main = paste("Position", i))
+    points(tv, d, col = "red")
+}
+
+y <- numeric()
+for (i in 1:3) {
+    s <- logical(3); s[i] <- TRUE
+    y <- c(y, dist.dna(sylvia.clus[, s], p = TRUE))
+}
+g <- gl(3, length(y) / 3)
+library(lattice)
+histogram(~ y | g, breaks = 20)
+
+nj.sylvia.K80 <- nj(syl.K80)
+nj.sylvia.GG95 <- nj(syl.GG95)
+dist.topo(nj.sylvia.K80, nj.sylvia.GG95)
+
+grep("Chamaea", taxa.sylvia, value = TRUE)
+f <- function(xx) root(nj(dist.dna(xx, p=TRUE)), "AJ534526")
+tr <- f(sylvia.clus)
+## same than: tr <- root(nj.sylvia.K80, "AJ534526")
+nj.boot.sylvia <- boot.phylo(tr, sylvia.clus, f, 200,
+                             rooted = TRUE)
+nj.boot.codon <- boot.phylo(tr, sylvia.clus, f, 200, 3,
+                            rooted = TRUE)
+nj.est <- tr
+nj.est$tip.label <- taxa.sylvia[tr$tip.label]
+plot(nj.est, no.margin = TRUE)
+nodelabels(round(nj.boot.sylvia / 200, 2), bg = "white")
+add.scale.bar(length = 0.01)
+write.tree(nj.est, "sylvia_nj_k80.tre")
+
+write.dna(sylvia.clus, "sylvia.txt")
+
+## ignore
+# phyml.sylvia <- phymltest("sylvia.txt", execname = "~/phyml")
+# summary(phyml.sylvia)
+# plot(phyml.sylvia, col = "black")
+# TR <- read.tree("sylvia.txt_phyml_tree.txt")
+# mltr.sylvia <- TR[[28]]
+# mltr.sylvia$tip.label <- taxa.sylvia[mltr.sylvia$tip.label]
+# mltr.sylvia <- root(mltr.sylvia, "Chamaea_fasciata")
+# plot(mltr.sylvia, no.margin = TRUE)
+# add.scale.bar(length = 0.01)
+## ignore
+
+# tr.ml <- drop.tip(mltr.sylvia, "Chamaea_fasciata")
+tr.ml <- drop.tip(nj.est, "Chamaea_fasciata")
+res <- vector("list", 9)
+for (L in -4:4)
+    res[[L + 5]] <- chronopl(tr.ml, 10^L, 12, 16, CV = TRUE)
+Lambda <- 10^(-4:4)
+CV <- sapply(res, function(x) sum(attr(x, "D2")))
+plot(Lambda, CV / 1e5, log = "x")
+
+sylvia.chrono <- res[[2]]
+rts <- attr(sylvia.chrono, "rates")
+summary(rts)
+
+par(mar = c(2, 0, 0, 0))
+plot(sylvia.chrono, edge.width = 100*rts, label.offset = .15)
+axisPhylo()
+write.tree(sylvia.chrono, "sylvia.chrono.tre")
+
+###
+### Chapter 6
+###
+
+load("sylvia.RData")
+nj.est <- read.tree("sylvia_nj_k80.tre")
+nj.est <- drop.tip(nj.est, "Chamaea_fasciata")
+DF <- sylvia.eco[nj.est$tip.label, ]
+# nj.est <- drop.tip(nj.est, "Curruca_melanocephala")
+# nj.est <- drop.tip(nj.est, "Curruca_conspicillata")
+# nj.est <- drop.tip(nj.est, "Curruca_subcoerulea")
+# DF <- DF[complete.cases(DF), ]
+table(DF$geo.range, DF$mig.behav)
+
+syl.er <- ace(DF$geo.range, nj.est, type = "d")
+syl.sym <- ace(DF$geo.range, nj.est, type="d", model="SYM")
+anova(syl.er, syl.sym)
+
+mod <- matrix(0, 3, 3)
+mod[2, 1] <- mod[1, 2] <- 1
+mod[2, 3] <- mod[3, 2] <- 2
+syl.mod <- ace(DF$geo.range, nj.est, type="d", model=mod)
+
+sapply(list(syl.er, syl.sym, syl.mod), AIC)
+
+Q <- syl.mod$index.matrix
+diag(Q) <- 0
+Q[1, 2] <- Q[2, 1] <- syl.mod$rates[1]
+Q[2, 3] <- Q[3, 2] <- syl.mod$rates[2]
+
+# Q[] <- c(0, syl.mod$rates)[Q + 1]
+Q[] <- c(0, syl.mod$rates)*(Q + 1)
+diag(Q) <- -rowSums(Q)
+
+P <- matexpo(0.05 * Q)
+rownames(P) <- c("temp", "temptrop", "trop")
+colnames(P) <- rownames(P)
+
+co <- rep("grey", 24)
+co[DF$geo.range == "temp"] <- "black"
+co[DF$geo.range == "trop"] <- "white"
+plot(nj.est, "c", FALSE, no.margin = TRUE, label.offset = 1)
+tiplabels(pch = 22, bg = co, cex = 2, adj = 1)
+nodelabels(thermo = syl.mod$lik.anc, cex = 0.8,
+           piecol = c("black", "grey", "white"))
+
+sylvia.chrono <- read.tree("sylvia.chrono.tre")
+yule(sylvia.chrono)
+birthdeath(sylvia.chrono)
+1 - pchisq(2*(-1.034112 - -1.113822), 1)
+
+x <- sylvia.eco[sylvia.chrono$tip.label, "geo.range"]
+ANC <- ace(x, sylvia.chrono, type = "d", model = mod)
+ANC$lik.anc[1:3, ]
+# --- start my changes
+# anc <- apply(ANC$lik.anc, 1, which.max)
+anc <- apply(ANC$lik.anc, 1, 
+             FUN = function(i){
+               factor(names(which.max(i)))
+             })
+anc <- as.character(anc)
+x <- as.character(x)
+X <- factor(c(x,anc), levels = c("trop","temp","temptrop"))
+x <- factor(x, levels = c("trop","temp","temptrop"))
+x <- factor(anc, levels = c("trop","temp","temptrop"))
+X <- X[!is.na(X)]
+# X <- factor(c(x, anc))
+# ---- end my chnages
+yule.cov(sylvia.chrono, ~ X)
+1 / (1 + exp(-(-0.0535529)))
+1 / (1 + exp(-(-0.0535529 -1.4608019)))
+1 / (1 + exp(-(-0.0535529 -0.9775966)))
+
+fsamp <- function(x) sample(length(x), size = 1, prob = x)
+nrep <- 1e3
+Pvls <- numeric(nrep)
+for (i in 1:nrep) {
+    anc <- apply(ANC$lik.anc, 1, fsamp)
+    # X <- factor(c(x, anc)) --- my change ----
+    Pvls[i] <- yule.cov(sylvia.chrono, ~ X)$Pval
+}
+hist(Pvls, freq = FALSE, main = "")
+lines(density(Pvls))
+
+## What is "Error in X[phy$edge[, 1], , drop = FALSE] : subscript out of bounds"
+
+
+
+# ------------------- Question 2 -----------------------------
+
+
+DF$geo.range
+ape::ace(DF$geo.range, phy = sylvia.chrono, type="discrete")
+ape::MPR(DF$geo.range, phy = sylvia.eco, outgroup="")
