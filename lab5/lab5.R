4cc8f3d40128a359e0dd524d0be80debbe7dd9e5
MartinSmel <43166833+MartinSmel@users.noreply.github.com>
2018-12-11 21:27:11 +0100

    LAB 5

---
+++ lab5/lab5.R
@@ -0,0 +1,21 @@
+install.packages("devtools")
+library(devtools)
+install_github("mategarb/R.ROSETTA")
+library(R.ROSETTA)
+View(autcon)
+data("autcon")
+table(autcon$decision)
+autconDefault = rosetta(autcon)
+autconDefault$main
+table(autconDefault$main$DECISION)
+autconDefault$quality
+#CV = 10
+#reducer = "Johnson
+#discreteMethod = "EqualFrequency"
+#discreteParam = 3
+#Mean accurancy = 0.821818
+autconDefault$main[1:3,]
+length(autconDefault$main[which(autconDefault$main$PVAL <0.05),])
+saveLineByLine(autconDefault$main, "rules.txt")
+
+
